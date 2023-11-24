cdbda39f9df3d5fd90f6c09f187bfaf2ae9c2696
Mach Ngoc Xuan <machngocxuan2004@gmail.com>
2023-11-24 17:21:19 +0700

    docker mysql2

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path=require("path");
 const configViewEngine=require("./config/viewEngine");
 const webRouter = require("./router/web");
+const mysql=require("mysql2");
+const { error } = require("console");
 
 const app = express()
 const port = process.env.PORT ||6969;
@@ -11,6 +13,22 @@ const hostname=process.env.HOST_NAME;
 
 configViewEngine(app);
 app.use("/",webRouter);
+
+const connetion=mysql.createConnection({
+  host:"localhost",
+  port:3307,
+  user:"root",
+  password:"123456",
+  database:"XuanIT"
+});
+
+connetion.query(
+  "select * from Users",
+  (err,results,fields)=>{
+    console.log("results=",results);
+    console.log("fields=",fields);
+  }
+)
 
 
 app.listen(port,hostname, () => {
